fdb56ac748606dcdce9529d85c175529a58f1e18
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-09-22 15:05:07 +0200

    Fix for stylesheet

--- examples/HtmlInlinerConsoleApp/Program.cs
+++ examples/HtmlInlinerConsoleApp/Program.cs
@@ -1,8 +1,8 @@
 ﻿using HtmlInliner;
 
 var htmlInliner = new HTMLInliner();
-var googleInlined = htmlInliner.Process("https://www.google.com");
-File.WriteAllText(@"c:\temp\web\google_inlined.htm", googleInlined);
+// var googleInlined = htmlInliner.Process("https://www.google.com");
+// File.WriteAllText(@"c:\temp\web\google_inlined.htm", googleInlined);
 
 var mstackInlined = htmlInliner.Process("https://www.mstack.nl");
 File.WriteAllText(@"c:\temp\web\mstack_inlined.htm", mstackInlined);
